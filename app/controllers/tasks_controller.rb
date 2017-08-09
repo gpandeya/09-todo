3df0be6a5b890024b7901ca770c04db87626b8d8
Gyanesh Pandeya <gyanesh.pandeya@gmail.com>
2017-08-09 21:39:24 +0000

    delete sample css

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,15 +12,20 @@ class TasksController < ApplicationController
         redirect_to tasks_url
     end
     
-    def new
+    def edit
+        @task = Task.find(params[:id])
+          @tasks = Task.order(id: :desc)
+        render 'index'
     end
     
-    def edit
-    end
+    
     def show
     end
   
     def update
+        task = Task.find(params[:id])
+        task.update(params[:task].permit(:title, :id, :is_complete, :due,:priority ,:category))
+        redirect_to tasks_url
     end
     
     def destroy
